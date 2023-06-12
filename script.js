c5d2d32c5f72e52202776ed2dd61e9e4547777b5
Tasnimul hasan <116421029+hashhacker7@users.noreply.github.com>
2023-06-12 12:16:31 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function red(){
+  document.getElementById("object").style.backgroundColor = "red";
+}
+function blue(){
+  document.getElementById("object").style.backgroundColor = "blue";
+}
+function green(){
+  document.getElementById("object").style.backgroundColor = "green";
+}
